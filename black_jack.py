7d012f553e063b3d28351ee5c905b2ff2f18f72d
sammychill <sammy@pharos.net>
2016-02-22 00:22:09 -0800

    updated w/ new pull, while loop works and shoe shuffles

--- black_jack.py
+++ black_jack.py
@@ -135,11 +135,7 @@ class Game(object):
         '''
 
     def play(self):
-<<<<<<< HEAD
-        deck = Shoe()
-=======
         shoe = Shoe()
->>>>>>> master
         hand = Hand()
         card = Card()
         score = 0 #determine how close to 21
@@ -148,20 +144,6 @@ class Game(object):
             shoe.move_cards(hand, 2)
             print "Here's your starting hand: %s" % (', '.join(map(str, hand.cards)))
             #making hand.cards list into string to print
-<<<<<<< HEAD
-            score = 0
-            while score < 50:
-                if raw_input("Would you like to hit? (Y) or (N) ") == 'Y':
-                    deck.move_cards(hand, 1)
-                    print "Now here's your hand: %s" % (', '.join(map(str, hand.cards)))
-                    for card in hand.cards:
-                        score += card.rank
-                else:
-                    #determine how close to 21
-                    for card in hand.cards:
-                        score += card.rank
-                    print "Your score: %d" % (score) #too high, indices of face cards wrong
-=======
             for card in hand.cards:
                 score += card.rank
             print "Your score is %d" % (score) #too high, indices of face cards wrong
@@ -173,16 +155,12 @@ class Game(object):
                 for card in hand.cards:
                     score += card.rank
                 print "Your score is %d" % (score) #too high, indices of face cards wrong
->>>>>>> master
         else:
             print "Guess you won't play with us :("
 
 game = Game()
 game.play()
-<<<<<<< HEAD
 
-=======
->>>>>>> master
 
 def find_defining_class(obj, method_name):
     """Finds and returns the class object that will provide
